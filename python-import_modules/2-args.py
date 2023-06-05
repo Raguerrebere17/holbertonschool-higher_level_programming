c6ee9d831ef23dde9e15aeffca92394a0004aa77
Raguerrebere17 <raguerrebere17@gmail.com>
2023-06-04 18:48:56 -0700

    Task number 2

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    count = 0
+    if len(argv) < 2:
+        print("0 arguments.")
+    else:
+        print("{} {}".format(len(argv) - 1,
+            "arguments:" if len(argv) > 2 else "argument:"))
+        for x in argv:
+            if count != 0:
+                print("{}: {}".format(count, x))
+                count += 1
