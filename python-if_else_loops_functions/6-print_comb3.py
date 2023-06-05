27673f7eb57ac47fb0955e9a05cb01e803cc883e
Raguerrebere17 <raguerrebere17@gmail.com>
2023-06-04 18:20:16 -0700

    Task number 6

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -2,4 +2,4 @@
 for y in range(0, 9):
     for x in range(y + 1, 10):
         print("{}{}{}".format(y, x, 
-            (", " if x < 10 and y < 8 else "\n")), end='')
+(", " if x < 10 and y < 8 else "\n")), end='')
