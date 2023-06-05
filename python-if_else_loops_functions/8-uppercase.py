b9b12a6ea459cde02b09f95146500192ba0c3720
Raguerrebere17 <raguerrebere17@gmail.com>
2023-06-04 18:22:39 -0700

    Task number 8

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for x in str:
+        if ord(x) >= 97 and ord(x) <=122:
+            x = ord(x) - 32
+        else:
+            x = ord(x)
+        print("{:c}".format(x), end='')
+        print("")
