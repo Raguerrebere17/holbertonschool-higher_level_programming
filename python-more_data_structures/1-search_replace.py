91be348b2e6a30b560000ae94067a60b31cbf425
Raguerrebere17 <raguerrebere17@gmail.com>
2023-06-11 17:47:18 -0700

    Typo solved

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def search_replace/my_list, search, replace):
+def search_replace(my_list, search, replace):
     new_list = list(my_list)
     for x in range(len(my_list)):
         if my_list[x] == search:
