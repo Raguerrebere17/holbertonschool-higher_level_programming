9e8bae809e2c8448b2b7912edfa698d9dbd5c882
Raguerrebere17 <raguerrebere17@gmail.com>
2023-06-11 17:45:55 -0700

    Typo solved

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
     s1, s2, comm = list(set(set_1)), list(set(set_2)), []
-    for x in range(len(s2)):
-        if s1[x] == s2[y]:
-            comm.append(s1[x])
+    for x in range(len(s1)):
+        for y in range(len(s2)):
+            if s1[x] == s2[y]:
+                comm.append(s1[x])
     return comm
